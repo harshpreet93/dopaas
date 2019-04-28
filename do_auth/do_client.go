f5f5eb12c870dfde21b3e39ee916edc6a5ef4129
Harshpreet Singh <singhharshpreet1993@gmail.com>
2019-04-27 22:24:03 -0700

    client provider code works now

--- do_auth/do_client.go
+++ do_auth/do_client.go
@@ -1,14 +1,16 @@
 package do_auth
 
 import (
+	"bytes"
 	"context"
 	"github.com/digitalocean/godo"
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
+	"io/ioutil"
+	"log"
+	"os"
 )
-
-//const (
-//	pat = "mytoken"
-//)
 
 type TokenSource struct {
 	AccessToken string
@@ -21,14 +23,33 @@ func (t *TokenSource) Token() (*oauth2.Token, error) {
 	return token, nil
 }
 
+func getFieldFromFile(filepath string, fieldName string) string {
+	viper.SetConfigType("yaml") // or viper.SetConfigType("YAML")
+
+	dat, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		log.Println("error reading yaml config ", err)
+	}
+	viper.ReadConfig(bytes.NewBuffer(dat))
+
+	return viper.Get(fieldName).(string) // this would be "steve"
+}
+
 func getTokenFromFS() string {
 	// get token from ~/.dopaas.yaml
 	// file format is:
 	// DIGITALOCEAN_ACCESS_TOKEN: "blahhhh"
-	return ""
+
+	dopaasConf, err := homedir.Expand("~/.dopaas.yaml")
+
+	if err != nil {
+		log.Println("error getting dopaas file", err)
+		os.Exit(1)
+	}
+	return getFieldFromFile(dopaasConf, "DIGITALOCEAN_ACCESS_TOKEN")
 }
 
-func auth() *godo.Client {
+func Auth() *godo.Client {
 	pat := getTokenFromFS()
 	tokenSource := &TokenSource{
 		AccessToken: pat,
