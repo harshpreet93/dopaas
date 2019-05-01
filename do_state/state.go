953355c8833e2ad844d4c5909145904cf440c6ae
Harshpreet Singh <singhharshpreet1993@gmail.com>
2019-04-30 21:09:46 -0700

    having trouble accessing the api due to network conditions so just pushing this

--- do_state/state.go
+++ do_state/state.go
@@ -3,23 +3,30 @@ package do_state
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/digitalocean/godo"
 	"github.com/harshpreet93/dopaas/do_auth"
 	"log"
 	"os"
 )
 
+type dropletInfo struct {
+	slug string
+	dc string
+	tags [] string
+}
+
 type projectState struct {
-	numDroplets int
+	numDroplets []dropletInfo
 }
 
 func getProject(projectId string) (*godo.Project, error) {
-
+	fmt.Println("finding project")
 	client := do_auth.Auth()
 	ctx := context.Background()
-
 	// create options. initially, these will be blank
 	opt := &godo.ListOptions{}
+
 	for {
 		projects, resp, err := client.Projects.List(ctx, opt)
 
@@ -50,7 +57,38 @@ func getProject(projectId string) (*godo.Project, error) {
 	return nil, errors.New("cannot find project with ID " + projectId)
 }
 
+
 func extractProjectResourceInfo(project *godo.Project) (*projectState, error) {
+	fmt.Println("extracting project resource info")
+	client := do_auth.Auth()
+	ctx := context.Background()
+	opt := &godo.ListOptions{}
+
+	for {
+		projectResources, resp, err := client.Projects.ListResources(ctx, project.ID, opt)
+
+		if err != nil {
+			return nil, err
+		}
+
+		for _, projectResource := range projectResources {
+			fmt.Println("project resource ", projectResource)
+		}
+
+		// if we are at the last page, break out the for loop
+		if resp.Links == nil || resp.Links.IsLastPage() {
+			break
+		}
+
+		page, err := resp.Links.CurrentPage()
+		if err != nil {
+			return nil, err
+		}
+
+		// set the page we want for the next request
+		opt.Page = page + 1
+	}
+
 	return nil, nil
 }
 
@@ -61,6 +99,7 @@ func GetState(projectId string) (*projectState, error) {
 
 	if err != nil {
 		log.Println("error getting project ", err)
+		os.Exit(1)
 	}
 
 	currState, err := extractProjectResourceInfo(project)
