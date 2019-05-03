dce70e2ddc2be6c30c73a53c5660f2596eebd271
Harshpreet Singh <singhharshpreet1993@gmail.com>
2019-05-02 23:00:14 -0700

    use naming and tags to do droplet->app mapping instead of projects

--- do_action/add_droplets.go
+++ do_action/add_droplets.go
@@ -6,7 +6,7 @@ import (
 	"github.com/harshpreet93/dopaas/conf"
 	"github.com/harshpreet93/dopaas/do_auth"
 	"log"
-	"os"
+	"strconv"
 )
 
 type AddDroplets struct {
@@ -16,23 +16,20 @@ type AddDroplets struct {
 	ImageSlug  string
 }
 
+func generateDropletNames(numDesired int) []string {
+	var names []string
+	for i := 0; i < numDesired; i++ {
+		names = append(names, conf.GetConfig().GetString("project_name")+"--"+(strconv.Itoa(i)))
+	}
+	return names
+}
+
 func (a AddDroplets) Execute() error {
 	ctx := context.Background()
-	dropletMultiCreateRequest := &godo.DropletMultiCreateRequest{Names: []string{"1", "2", "3"}, Region: a.Region, Size: a.SizeSlug, Image: godo.DropletCreateImage{Slug: a.ImageSlug}}
-	droplets, _, err := do_auth.Auth().Droplets.CreateMultiple(ctx, dropletMultiCreateRequest)
+	dropletMultiCreateRequest := &godo.DropletMultiCreateRequest{Names: generateDropletNames(conf.GetConfig().GetInt("NumDroplets")), Region: a.Region, Size: a.SizeSlug, Image: godo.DropletCreateImage{Slug: a.ImageSlug}}
+	_, _, err := do_auth.Auth().Droplets.CreateMultiple(ctx, dropletMultiCreateRequest)
 	if err != nil {
 		log.Println("Error adding droplets ", err)
 	}
-	assignDropletsToProject(droplets, conf.GetConfig().GetString("project_id"))
 	return nil
-}
-
-func assignDropletsToProject(droplets []godo.Droplet, projectId string) {
-		ctx := context.Background()
-		projectsService := &godo.ProjectsServiceOp{do_auth.Auth() }
-		_, _, err := projectsService.AssignResources(ctx, projectId, droplets)
-		if err != nil {
-			log.Println("error assigning resources to project ", err)
-			os.Exit(1)
-		}
 }
